37b28dd2917d55f0fe9292c5bc0cb4e76f7b2a7d
satk0 <satko_a@op.pl>
2024-05-19 19:20:13 +0200

    Make multiple renamings possible

--- src/main.c
+++ src/main.c
@@ -4,20 +4,31 @@
 
 #include <r_core.h>
 
-// TODO:
-// 1. Store multiple renamings, not only one as of right now.
-// 2. Make it accept multispace quoted arguments, i.e.: "rbp + rax - 0x1024".
-
-static char* old_name_str;
-static char* new_name_str;
+static RList *old_names;
+static RList *new_names;
 
 // afen parser
 static int r_afen_parse(RParse *p, const char *data, char *str) {
 	int res = true;
 	char *input = strdup (data);
 
-	if (old_name_str && new_name_str)
-		input = r_str_replace_all (input, old_name_str, new_name_str);
+	int n = r_list_length(old_names);
+
+	if (n){
+		RListIter *s_iter_old = NULL;
+		RListIter *s_iter_new = NULL;
+
+		s_iter_old = old_names->head;
+		s_iter_new = new_names->head;
+
+		for (int i = 0; i < n; ++i) {
+			input = r_str_replace_all (input, s_iter_old->data, s_iter_new->data);
+
+			s_iter_old = r_list_iter_get_next(s_iter_old);
+			s_iter_new = r_list_iter_get_next(s_iter_new);
+		}
+
+	}
 
 	strcpy (str, input);
 	return res;
@@ -27,9 +38,8 @@ static int r_afen_parse(RParse *p, const char *data, char *str) {
 static int r_cmd_init(void *user, const char *input) {
 	RCmd *rcmd = (RCmd *) user;
 	RCore *core = (RCore *) rcmd->data;
-
-	old_name_str = (char*) r_malloc(50 * sizeof(char));
-	new_name_str = (char*) r_malloc(50 * sizeof(char));
+	old_names = r_list_new ();
+	new_names = r_list_new ();
 
 	core->parser->cur->parse = r_afen_parse;
 	
@@ -37,8 +47,8 @@ static int r_cmd_init(void *user, const char *input) {
 }
 
 static int r_cmd_fini(void *user, const char *input) {
-	r_free(old_name_str);
-	r_free(new_name_str);
+	r_list_free(old_names);
+	r_list_free(new_names);
 
 	return true;
 }
@@ -47,30 +57,14 @@ static int r_cmd_afen_client(void *user, const char *input) {
 	if (r_str_startswith (input, "afen")) {
 		int *argc = (int*) malloc(sizeof(int));
 		char **argv = r_str_argv(input, argc);
-		//r_cons_printf("num of args: %d", *argc);
-		//r_cons_printf("arg 1: %s", argv[1]);
-
-		/*
-		RList *splitted = r_str_split_list((char*)input, " ", 3);
-		int num_of_spaces = r_list_length(splitted);
-		*/
 
 		if (*argc != 3) {
 			r_cons_printf("Usage: afen new_name old_name\n");
 			return true;
 		}
 
-		//RListIter *s_iter = NULL;
-		//s_iter = splitted->head;
-
-		//RListIter *new_name = r_list_iter_get_next(s_iter);
-		//RListIter *old_name = r_list_iter_get_next(new_name);
-
-		strcpy(new_name_str, argv[1]);
-		strcpy(old_name_str, argv[2]);
-
-		r_cons_printf ("old_name = %s\n", (char*) old_name_str);
-		r_cons_printf ("new_name = %s\n", (char*) new_name_str);
+		r_list_append(new_names, argv[1]);
+		r_list_append(old_names, argv[2]);
 
 		return true;
 	}
